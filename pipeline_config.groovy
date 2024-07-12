5e32b8a6affed51690713c54b9290a8b40a605ac
MarcelinoMajerhua <xt9099@rimac.com.pe>
2024-07-12 15:47:34 -0500

    feat: add config jenkins

---
+++ pipeline_config.groovy
@@ -0,0 +1,47 @@
+pipeline {
+    agent any
+
+    environment {
+        // Definir variables de entorno para AWS
+        AWS_ACCESS_KEY_ID = ''
+        AWS_SECRET_ACCESS_KEY = ''
+    }
+
+    stages {
+        stage('Input Credentials') {
+            steps {
+                script {
+                    // Solicitar AWS Access Key y Secret Key
+                    AWS_ACCESS_KEY_ID = input message: 'Ingrese su AWS Access Key ID', parameters: [string(defaultValue: '', description: 'AWS Access Key ID', name: 'AWS_ACCESS_KEY_ID')]
+                    AWS_SECRET_ACCESS_KEY = input message: 'Ingrese su AWS Secret Access Key', parameters: [password(defaultValue: '', description: 'AWS Secret Access Key', name: 'AWS_SECRET_ACCESS_KEY')]
+                }
+            }
+        }
+
+        stage('Install Dependencies') {
+            steps {
+                // Instalar dependencias
+                sh 'npm install'
+            }
+        }
+
+        stage('Deploy to AWS') {
+            steps {
+                // Configurar credenciales de AWS
+                withEnv(["AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}", "AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}"]) {
+                    // Desplegar con Serverless Framework
+                    sh 'npx serverless deploy'
+                }
+            }
+        }
+    }
+
+    post {
+        success {
+            echo 'Deployment Successful!'
+        }
+        failure {
+            echo 'Deployment Failed!'
+        }
+    }
+}
